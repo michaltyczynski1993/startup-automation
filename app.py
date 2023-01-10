be8307448c9d77ac30d69b613c70afe6111d3892
Michal Tyczynski <m.tyczynski1993@gmail.com>
2023-01-10 17:30:19 +0100

    main function

--- app.py
+++ app.py
@@ -4,15 +4,21 @@ import os
 exe_str = "exe"
 pdf_str = "pdf"
 excel_str = "xlsx"
+file = '.\data.txt'
 
-# read txt file and translate it into variable
-with open('data.txt', encoding="utf-8") as f:
-    for path in f:
-        try:
-            os.system(path)
-        except:
-            print("Podana scierzka nie istnieje")
-        
-f.close()
-input("Press any key to continue ...")
+def main():
+    # read txt file and translate it into variable
+    with open(file, encoding="utf-8") as f:
+        for path in f:
+            try:
+                os.system(path)
+                print(path)
+            except:
+                print("Directory not existing")
+    
+    f.close()
+    input("Press any key to close program ...")
+
+if __name__ == "__main__":
+    main()
 
